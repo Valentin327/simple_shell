6de511292e6cea00a5c6c1d19d6711708a36f6dd
Valentin ZONGO <valentizongo@gmail.com>
2022-06-15 15:19:37 +0000

    added _strcat function

--- func_methods.c
+++ func_methods.c
@@ -116,3 +116,30 @@ char *_strtok(char *strToDiv, char *delim)
 	}
 	return (copystr);
 }
+
+/**
+ * _strcat - function that concatenates two string
+ * @dest: string to copy to
+ * @src: string to copy
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+	int lengthSrc = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	lengthSrc = _strlen(src);
+	dest = _realloc(dest, i, j, lengthSrc + i + 1);
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
